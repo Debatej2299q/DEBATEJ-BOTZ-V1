f78ab58e0f9c0c0100fd87beb0f2aa01e9f3adc6
⚡𝘿 𝞢 𝞑 𝞓 𝙏 𝞢 𝙅🍷🕊️ <156593033+Debatej2299q@users.noreply.github.com>
2025-07-12 08:57:09 +0530

    Create config.js

---
+++ bot/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  prefix: "!",
+  ownerNumber: process.env.OWNER_NUMBER || "YOUR_OWNER_NUMBER",
+  mainGroupId: process.env.MAIN_GROUP_ID || "YOUR_MAIN_GROUP_ID",
+  supportGroupId: process.env.SUPPORT_GROUP_ID || "YOUR_SUPPORT_GROUP_ID",
+  menuPicUrl: "https://your_menu_image_url.jpg",
+  alivePicUrl: "https://your_alive_image_url.jpg"
+};
